7bd0b96abd6581d884fae29a181fa683be7094c7
Neil <62321203+CestMerNeil@users.noreply.github.com>
2022-10-27 17:29:38 +0200

    Add files via upload
    TP-JAVA

---
+++ TP_JAVA/Eclipse_Workspace/ProjetJava-2022/src/projectJava_2022_Test/TestClass.java
@@ -0,0 +1,9 @@
+package projectJava_2022_Test;
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Coucou");
+		System.out.println("Coucou");
+	}
+}
---
+++ TP_JAVA/Eclipse_Workspace/ProjetJava-2022/src/projectJava_2022_Test/UnitTest.java
@@ -0,0 +1,14 @@
+package projectJava_2022_Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
