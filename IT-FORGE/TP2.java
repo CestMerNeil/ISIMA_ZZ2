3a16d93343de9e7948166ea10edefb93abaa443a
CestMerNeil <1453120442@qq.com>
2022-10-29 15:09:12 +0200

    IT-FORGE

---
+++ IT-FORGE/TP2.java
@@ -0,0 +1,27 @@
+public class TP2
+{
+    public static void main(String[] args)
+    {
+        indeKilo test = new indeKilo();
+        test.trajet = 20;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        /*
+        test.trajet = -10;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 0;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 0.1;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 17.123;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 39.5;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 61;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 81;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        test.trajet = 99;
+        System.out.println("Test of "+test.trajet+"Km, Rembourse is "+test.getRembourse()+" euros");
+        */
+    }
+}
